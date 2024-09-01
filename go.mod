8f55dc660eb48f238614309a8a34e2aa34c9b088
NF4Caniago <afifilhamcaniago@gmail.com>
2024-09-02 00:39:49 +0700

    done

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/NF4Caniago/golang-pzn-app-modules
 
 go 1.22.5
 
-require github.com/NF4Caniago/golang-pzn-modules v1.0.1
+require github.com/NF4Caniago/golang-pzn-modules/v4 v4.0.0 // indirect
--- main.go
+++ main.go
@@ -3,9 +3,9 @@ package main
 import (
 	"fmt"
 
-	go_say_hello "github.com/NF4Caniago/golang-pzn-modules"
+	go_say_hello "github.com/NF4Caniago/golang-pzn-modules/v4"
 )
 
 func main() {
-	fmt.Println(go_say_hello.SayHello())
+	fmt.Println(go_say_hello.SayHello("afif"))
 }
